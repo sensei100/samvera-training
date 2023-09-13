b6587d3831b2628d5dd611dc0d94e7c0b2f3e653
sensei100 <tuh42082@temple.edu>
2023-09-13 13:58:20 -0400

    Added form properties

--- app/forms/hyrax/thesis_form.rb
+++ app/forms/hyrax/thesis_form.rb
@@ -4,6 +4,8 @@ module Hyrax
   # Generated form for Thesis
   class ThesisForm < Hyrax::Forms::WorkForm
     self.model_class = ::Thesis
-    self.terms += [:resource_type]
+    self.terms += [:resource_type, :contact_email, :contact_phone, :department]
+    self.required_fields += [:department, :contact_email]
+    self.required_fields -= [:keyword, :rights]
   end
 end
--- app/models/thesis.rb
+++ app/models/thesis.rb
@@ -3,6 +3,18 @@
 class Thesis < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
 
+  property :contact_email, predicate: ::RDF::Vocab::VCARD.hasEmail, multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :contact_phone, predicate: ::RDF::Vocab::VCARD.hasTelephone, multiple: true do |index|
+    index.as :stored_searchable
+  end
+
+  property :department, predicate: ::RDF::URI.new("http://lib.my.edu/departments"), multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
   self.indexer = ThesisIndexer
   # Change this to restrict which works can be added as a child.
   # self.valid_child_concerns = []
